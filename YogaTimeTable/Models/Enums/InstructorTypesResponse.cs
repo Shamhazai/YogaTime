188c253312b1225babcf47f9e4fffe936575852d
Shamhazai <106805767+Shamhazai@users.noreply.github.com>
2023-11-09 21:10:44 +0300

    Перенесла файлы

---
+++ YogaTimeTable/Models/Enums/InstructorTypesResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace YogaTimeTable.Models.Emams
+{
+    public enum InstructorTypesResponse
+    {
+        /// <summary>
+        /// Клиент
+        /// </summary>
+        Client,
+
+        /// <summary>
+        /// Инструктор
+        /// </summary>
+        Instructor
+    }
+}
